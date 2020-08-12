a99476d37d0030eaa55644629e08dade424bd6e1
Андрей Шарунов <dron84@gmail.com>
2020-08-12 17:44:22 +0300

    up

--- src/components/foot.vue
+++ src/components/foot.vue
@@ -1,19 +1,24 @@
 <template>
-    <footer class="footer">
-        <center>
-            <iframe src="https://money.yandex.ru/quickpay/shop-widget?writer=seller&targets=donate&targets-hint=&default-sum=&button-text=11&hint=&successURL=&quickpay=shop&account=4100193714503" style="width: 290px; max-width: 450px; height: 222px;" frameborder="0" allowtransparency="true" scrolling="no"></iframe>
-            <h1>uniquesite.ru</h1>
-            <p>©2012-2017 «uniquesite»</p>
-        </center>
-    </footer>
+  <footer class="footer">
+    <center>
+      <iframe
+        src="https://money.yandex.ru/quickpay/shop-widget?writer=seller&targets=donate&targets-hint=&default-sum=&button-text=11&hint=&successURL=&quickpay=shop&account=4100193714503"
+        style="width: 290px; max-width: 450px; height: 222px;"
+        frameborder="0"
+        allowtransparency="true"
+        scrolling="no"
+      ></iframe>
+      <h1>uniquesite.ru</h1>
+      <p>©2012-2020 «uniquesite»</p>
+    </center>
+  </footer>
 </template>
 
 <script>
-    export default {
-        name: "foot"
-    }
+export default {
+  name: "foot",
+};
 </script>
 
 <style scoped>
-
 </style>
